f288fd5d0a16b7ef20a4b9fd9466ca324cfdecf7
Yusheng Wu <wuyusheng1023@gmail.com>
2021-04-08 00:27:19 +0300

    adjust mock mini cpc

--- worker/app/model.py
+++ worker/app/model.py
@@ -9,18 +9,18 @@ class MockMiniCpc:
     self.to_set = 42;
     self.flow_set = 200;
 
-    self.temp_sat = 20;
+    self.temp_sat = 40;
     self.temp_con = 20;
-    self.temp_opt = 20;
-    self.flow = 0;
-    self.conc = 0;
+    self.temp_opt = 42;
+    self.flow = 200;
+    self.conc = 1000;
   
   def __repr__(self):
     return f"{self.temp_sat} {self.temp_con} {self.temp_opt} {self.flow} {self.conc}"
 
   @staticmethod
-  def _update(old, _set, ratio=0.9, fluctuation=0.05):
-    return old * ratio + (_set - old) * (1 - ratio) + (random() - 0.5) * fluctuation * _set
+  def _update(old, _set, ratio=0.9, fluctuation=0.02):
+    return old * ratio + _set * (1 - ratio) + (random() - 0.5) * fluctuation * _set
   
   def update(self):
     self.temp_sat = self._update(self.temp_sat, self.ts_set)
